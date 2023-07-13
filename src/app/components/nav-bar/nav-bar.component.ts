b4a9a8e8eeab8882de3a205604860ec372abe31b
Hanns Espinoza <hanssabinadie@gmai.com>
2023-07-13 00:45:32 -0600

    feat: :construction: add navbar component

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nav-bar-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent {
+
+}
--- src/app/pages/layout-page/layout-page.component.ts
+++ src/app/pages/layout-page/layout-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarComponent } from '@components/index';
 
 @Component({
   selector: 'app-layout-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavBarComponent],
   templateUrl: './layout-page.component.html',
   styleUrls: ['./layout-page.component.css']
 })
