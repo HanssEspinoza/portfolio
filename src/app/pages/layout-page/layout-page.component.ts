ba06ad46165090e33ca25de8ada1ac18c5cbfc14
Hanns Espinoza <hanssabinadie@gmai.com>
2023-07-13 00:39:54 -0600

    feat: :construction: add layoutPage

---
+++ src/app/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-layout-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './layout-page.component.html',
+  styleUrls: ['./layout-page.component.css']
+})
+export class LayoutPageComponent {
+
+}
